6cfef16b28e19283de345728d3d66ac751ee46ec
rirumon <islamrumon420@gmail.com>
2019-10-06 11:40:15 +0600

    Add the Drawer in apps just design and implement index tab

---
+++ lib/helper/helper.dart
@@ -0,0 +1,6 @@
+
+//Drawer design
+import 'package:flutter/material.dart';
+
+final drawerEntityColor = Colors.white;
+final drawerTextStyle = TextStyle(fontWeight: FontWeight.bold,color: drawerEntityColor,fontSize: 16);
